36e7d34d950cf0599f4978365d4a413d9c6f2c09
stud-14 <pashayevan>
2024-10-01 10:27:02 +0400

    add .

---
+++ 1okt.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <unordered_set>
+#include <algorithm>
+
+std::string cleanWord(const std::string& word) {
+	std:: string cleaned:
+	for char c:word) {
+		if
+
+int main() {
+	std::unordered_set<std::string> words ;
+	std::set<std::string> dublicate_words ;
+	std::string word;
+	int count=0;
+	
+	
+	while(std::cin>>word)
+	{
+		if(word=="love") {
+			count++;
+		}
+		if(words.contains(word))
+		{
+			dublicate_words.insert(word);
+		}
+		else
+		{words.insert(word);
+		}
+	}
+	std::cout<<" "<<count<<"\n";
+	return 0;
+}
